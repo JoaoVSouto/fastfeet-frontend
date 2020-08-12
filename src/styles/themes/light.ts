496fdd995c0977a5fa36d30b692ac3fd771ede3c
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-12 09:57:33 -0300

    Adds color palette

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,24 @@
+export default {
+  primary: '#7d40e7',
+  secondary: '#fff',
+  tertiary: '#a991c5',
+
+  titleColor: '#444',
+  textColor: '#666',
+  textSecondaryColor: '#999',
+
+  borderColor: '#ddd',
+  iconColor: '#c6c6c6',
+
+  success: '#2ca42b',
+  successLight: '#dff0df',
+
+  warning: '#c1bc35',
+  warningLight: '#f0f0df',
+
+  info: '#4d85ee',
+  infoLight: '#bad2ff',
+
+  danger: '#de3b3b',
+  dangerLight: '#fab0b0',
+};
