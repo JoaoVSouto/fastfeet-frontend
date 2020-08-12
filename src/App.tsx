282adfc18f033ee024b9704185d5e24f37b1e414
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-12 09:05:45 -0300

    Creates global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1>hello world</h1>
+
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: none;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body,
+  input,
+  button {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
