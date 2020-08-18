408347b2567647c5fcbb950a231d57902e9afde5
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-18 14:54:00 -0300

    Styles search input

--- src/pages/PackagesDashboard/index.tsx
+++ src/pages/PackagesDashboard/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MdAdd } from 'react-icons/md';
+import { MdAdd, MdSearch } from 'react-icons/md';
 
 import {
   Container,
   Title,
   Controls,
+  SearchContainer,
   SearchInput,
   RegisterLink,
 } from './styles';
@@ -16,7 +17,10 @@ const PackagesDashboard: React.FC = () => {
       <Title>Gerenciando encomendas</Title>
 
       <Controls>
-        <SearchInput />
+        <SearchContainer>
+          <MdSearch />
+          <SearchInput type="text" placeholder="Buscar por encomendas" />
+        </SearchContainer>
         <RegisterLink as={Link} to="/packages/register">
           <MdAdd />
           Cadastrar
--- src/pages/PackagesDashboard/styles.ts
+++ src/pages/PackagesDashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { ButtonLink } from '../../components/Button';
+import Input from '../../components/Input';
 
 export const Container = styled.main`
   padding: 3.4rem 12rem;
@@ -18,7 +19,36 @@ export const Controls = styled.div`
   justify-content: space-between;
 `;
 
-export const SearchInput = styled.input``;
+export const SearchContainer = styled.div`
+  position: relative;
+
+  &:focus-within {
+    svg {
+      color: ${({ theme }) => theme.primary};
+    }
+  }
+
+  svg {
+    position: absolute;
+    left: 1.6rem;
+    top: 50%;
+    transform: translateY(-50%) scale(1.4);
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.textSecondaryColor};
+    transition: color 0.3s;
+  }
+`;
+
+export const SearchInput = styled(Input)`
+  height: 3.6rem;
+  padding-left: 4rem;
+  padding-right: 1.5rem;
+  font-size: 1.4rem;
+
+  &::placeholder {
+    font-size: inherit;
+  }
+`;
 
 export const RegisterLink = styled(ButtonLink)`
   display: flex;
