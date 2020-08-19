f6932c04646792580149061a7b2a3d20b07be79c
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-19 10:38:47 -0300

    Makes default layout occupies all height available

--- src/pages/_layouts/default/styles.ts
+++ src/pages/_layouts/default/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100%;
+  min-height: 100%;
+  overflow: hidden;
   background-color: ${({ theme: { secondaryDark } }) => secondaryDark};
 `;
