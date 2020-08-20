7599a87a2a804858d7d40db96462822a56abe0b4
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-20 17:57:50 -0300

    Styles packages cards

--- src/pages/PackagesDashboard/styles.ts
+++ src/pages/PackagesDashboard/styles.ts
@@ -80,11 +80,36 @@ export const CardsContainer = styled.section`
   display: none;
 
   ${down('tablet')} {
-    display: block;
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    margin-top: 2.4rem;
   }
 `;
 
-export const Card = styled.article``;
+export const Card = styled.article`
+  background-color: ${({ theme }) => theme.secondary};
+  border-radius: 0.4rem;
+  padding: 2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.14);
+
+  .card-row {
+    padding: 1rem;
+    border-radius: 0.4rem;
+
+    &:nth-child(2n) {
+      background-color: #eeeeee5e;
+    }
+
+    + .card-row {
+      margin-top: 1rem;
+    }
+
+    strong {
+      margin-right: 1rem;
+    }
+  }
+`;
 
 export const Table = styled.table`
   width: 100%;
