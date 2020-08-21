d96ada0648ef31b08528804a483073bd8b64732a
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-21 08:56:56 -0300

    Conditionally renders the table or cards based on window width

--- src/pages/PackagesDashboard/index.tsx
+++ src/pages/PackagesDashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import {
   MdAdd,
@@ -10,6 +10,8 @@ import {
 
 import { randomTheme } from '../../utils/getRandomTheme';
 import { getNameInitials } from '../../utils/getNameInitials';
+
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 import api from '../../services/api';
 
@@ -54,6 +56,20 @@ export interface IPackage {
 const PackagesDashboard: React.FC = () => {
   const [packages, setPackages] = useState<IPackage[]>([]);
 
+  const { width } = useWindowSize();
+
+  const isDesktop = useMemo(() => {
+    function isDesktopScreen(): boolean {
+      return width >= 992;
+    }
+
+    if (isDesktopScreen()) {
+      return true;
+    }
+
+    return false;
+  }, [width]);
+
   useEffect(() => {
     (async () => {
       const response = await api.get('packages');
@@ -76,48 +92,73 @@ const PackagesDashboard: React.FC = () => {
         </RegisterLink>
       </Controls>
 
-      <Table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Destinatário</th>
-            <th>Entregador</th>
-            <th>Cidade</th>
-            <th>Estado</th>
-            <th>Status</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
+      {isDesktop ? (
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Destinatário</th>
+              <th>Entregador</th>
+              <th>Cidade</th>
+              <th>Estado</th>
+              <th>Status</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
 
-        <tbody>
+          <tbody>
+            {packages.map(pkg => (
+              <tr key={pkg.id}>
+                <td>{`#${String(pkg.id).padStart(2, '0')}`}</td>
+                <td>{pkg.recipient.name}</td>
+                <td>
+                  <span>
+                    <ImageContainer>
+                      {pkg.courier.avatar ? (
+                        <img
+                          src={pkg.courier.avatar.url}
+                          alt={pkg.courier.name}
+                        />
+                      ) : (
+                        <ImagePlaceholder colorTheme={randomTheme()}>
+                          {getNameInitials(pkg.courier.name)}
+                        </ImagePlaceholder>
+                      )}
+                    </ImageContainer>
+                    {pkg.courier.name}
+                  </span>
+                </td>
+                <td>{pkg.recipient.city}</td>
+                <td>{pkg.recipient.uf}</td>
+                <td>
+                  <Status status={pkg.status}>{pkg.status}</Status>
+                </td>
+                <td>
+                  <Actions>
+                    <button type="button" className="view">
+                      <MdRemoveRedEye />
+                      Visualizar
+                    </button>
+                    <a href="#!" className="edit">
+                      <MdEdit />
+                      Editar
+                    </a>
+                    <button type="button" className="delete">
+                      <MdDeleteForever />
+                      Excluir
+                    </button>
+                  </Actions>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <CardsContainer>
           {packages.map(pkg => (
-            <tr key={pkg.id}>
-              <td>{`#${String(pkg.id).padStart(2, '0')}`}</td>
-              <td>{pkg.recipient.name}</td>
-              <td>
-                <span>
-                  <ImageContainer>
-                    {pkg.courier.avatar ? (
-                      <img
-                        src={pkg.courier.avatar.url}
-                        alt={pkg.courier.name}
-                      />
-                    ) : (
-                      <ImagePlaceholder colorTheme={randomTheme()}>
-                        {getNameInitials(pkg.courier.name)}
-                      </ImagePlaceholder>
-                    )}
-                  </ImageContainer>
-                  {pkg.courier.name}
-                </span>
-              </td>
-              <td>{pkg.recipient.city}</td>
-              <td>{pkg.recipient.uf}</td>
-              <td>
-                <Status status={pkg.status}>{pkg.status}</Status>
-              </td>
-              <td>
-                <Actions>
+            <Card key={pkg.id}>
+              <ActionsContainer>
+                <Actions isMobile>
                   <button type="button" className="view">
                     <MdRemoveRedEye />
                     Visualizar
@@ -131,73 +172,50 @@ const PackagesDashboard: React.FC = () => {
                     Excluir
                   </button>
                 </Actions>
-              </td>
-            </tr>
+              </ActionsContainer>
+
+              <div className="card-row">
+                <strong>ID</strong>
+                {`#${String(pkg.id).padStart(2, '0')}`}
+              </div>
+
+              <div className="card-row">
+                <strong>Destinatário</strong>
+                {pkg.recipient.name}
+              </div>
+
+              <div className="card-row">
+                <strong>Entregador</strong>
+                <ImageContainer>
+                  {pkg.courier.avatar ? (
+                    <img src={pkg.courier.avatar.url} alt={pkg.courier.name} />
+                  ) : (
+                    <ImagePlaceholder colorTheme={randomTheme()}>
+                      {getNameInitials(pkg.courier.name)}
+                    </ImagePlaceholder>
+                  )}
+                </ImageContainer>
+                {pkg.courier.name}
+              </div>
+
+              <div className="card-row">
+                <strong>Cidade</strong>
+                {pkg.recipient.city}
+              </div>
+
+              <div className="card-row">
+                <strong>Estado</strong>
+                {pkg.recipient.uf}
+              </div>
+
+              <div className="card-row">
+                <strong>Status</strong>
+                <Status status={pkg.status}>{pkg.status}</Status>
+              </div>
+            </Card>
           ))}
-        </tbody>
-      </Table>
-
-      <CardsContainer>
-        {packages.map(pkg => (
-          <Card key={pkg.id}>
-            <ActionsContainer>
-              <Actions isMobile>
-                <button type="button" className="view">
-                  <MdRemoveRedEye />
-                  Visualizar
-                </button>
-                <a href="#!" className="edit">
-                  <MdEdit />
-                  Editar
-                </a>
-                <button type="button" className="delete">
-                  <MdDeleteForever />
-                  Excluir
-                </button>
-              </Actions>
-            </ActionsContainer>
-
-            <div className="card-row">
-              <strong>ID</strong>
-              {`#${String(pkg.id).padStart(2, '0')}`}
-            </div>
-
-            <div className="card-row">
-              <strong>Destinatário</strong>
-              {pkg.recipient.name}
-            </div>
-
-            <div className="card-row">
-              <strong>Entregador</strong>
-              <ImageContainer>
-                {pkg.courier.avatar ? (
-                  <img src={pkg.courier.avatar.url} alt={pkg.courier.name} />
-                ) : (
-                  <ImagePlaceholder colorTheme={randomTheme()}>
-                    {getNameInitials(pkg.courier.name)}
-                  </ImagePlaceholder>
-                )}
-              </ImageContainer>
-              {pkg.courier.name}
-            </div>
-
-            <div className="card-row">
-              <strong>Cidade</strong>
-              {pkg.recipient.city}
-            </div>
-
-            <div className="card-row">
-              <strong>Estado</strong>
-              {pkg.recipient.uf}
-            </div>
-
-            <div className="card-row">
-              <strong>Status</strong>
-              <Status status={pkg.status}>{pkg.status}</Status>
-            </div>
-          </Card>
-        ))}
-      </CardsContainer>
+        </CardsContainer>
+      )}
     </Container>
   );
 };
